cf9d27ca7e8d15ce0f1282cca50c6a4e840a1c48
Bodale Paul <100213388+BPaul01@users.noreply.github.com>
2023-03-17 17:05:20 +0200

    Add files via upload

---
+++ Lab4/SecurityComponent.java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+
+public class SecurityComponent extends UsersDataBase {
+
+  public boolean connectionSecure;
+
+    public Vector  mySignInController;
+    public Vector  myUsersDataBase;
+
+  public void requestSecureConnection() {
+  }
+
+}
---
+++ Lab4/SignInController.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+
+public class SignInController extends AplicationSystemConnection {
+
+  public boolean loggedIn;
+
+    public Vector  myUsersDataBase;
+    public Vector  mySecurityComponent;
+
+  public String getUserToken(String userName) {
+  return null;
+  }
+
+  public boolean authentificateUser(String password, String userName) {
+  return false;
+  }
+
+  public boolean validateUserName(String userName) {
+  return false;
+  }
+
+}
